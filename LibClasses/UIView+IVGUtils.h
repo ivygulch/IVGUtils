d3d87ce5be3070335c10735f4aef2ac2a63d70ed
Douglas Sjoquist <dwsjoquist@sunetos.com>
2013-07-08 20:39:36 -0400

    update lib

--- LibClasses/UIView+IVGUtils.h
+++ LibClasses/UIView+IVGUtils.h
@@ -8,21 +8,80 @@
 
 #import <UIKit/UIKit.h>
 
+enum UIViewVerticalAlignment
+{
+	UIViewVerticalAlignmentNone = 0,
+    UIViewVerticalAlignmentTop,
+    UIViewVerticalAlignmentCenter,
+    UIViewVerticalAlignmentFull,
+    UIViewVerticalAlignmentBottom
+};
+typedef enum UIViewVerticalAlignment UIViewVerticalAlignment;
+
+enum UIViewHorizontalAlignment
+{
+	UIViewHorizontalAlignmentNone = 0,
+    UIViewHorizontalAlignmentLeft,
+    UIViewHorizontalAlignmentCenter,
+    UIViewHorizontalAlignmentFull,
+    UIViewHorizontalAlignmentRight
+};
+typedef enum UIViewHorizontalAlignment UIViewHorizontalAlignment;
+
 @interface UIView(IVGUtils)
 
+// needs to be a weak reference so we avoid a retain cycle between the view and it's controller/owner
+@property (nonatomic,weak) id viewOwner;
+@property (nonatomic,assign) CGPoint frameOrigin;
+@property (nonatomic,assign) CGFloat frameX;
+@property (nonatomic,assign) CGFloat frameY;
+@property (nonatomic,assign) CGSize frameSize;
+@property (nonatomic,assign) CGFloat frameWidth;
+@property (nonatomic,assign) CGFloat frameHeight;
+
 - (NSString *) superviewDescription;
-- (id) findSuperviewIsKindOfClass: (Class) clazz;
+- (id) findFirstViewInHierarchyWithKindOfClass: (Class) clazz;
 - (void) logSizes:(NSString *) desc;
 - (void) removeAllGestureRecognizers;
-- (void) removeAllSubviews;
 - (void) setOriginYBelow:(UIView *) baseView withGap:(CGFloat) gap;
 - (CGFloat) bottom;
 - (CGFloat) bottomOfLowestSubview;
 - (CGFloat) topOfHighestSubview;
+- (CGRect) absoluteFrame;
+- (CGPoint) absoluteOrigin;
+- (void) removeAllSubviews;
 
 + (void) updateFrame:(UIView *) view x:(CGFloat) x;
 + (void) updateFrame:(UIView *) view w:(CGFloat) w;
 + (CGFloat) bottomOfLowestView:(NSArray *) views;
 + (CGFloat) topOfHighestView:(NSArray *) views;
++ (id) findFirstViewInHiearchyWith:(id) item kindOfClass: (Class) clazz;
+
+- (UIView *) findFirstResponder;
+
+- (void) positionSubview:(UIView *) view horizontalAlignment:(UIViewHorizontalAlignment) horizontalAlignment verticalAlignment:(UIViewVerticalAlignment) verticalAlignment width:(CGFloat) width height:(CGFloat) height;
+- (void) positionSubview:(UIView *) view horizontalAlignment:(UIViewHorizontalAlignment) horizontalAlignment verticalAlignment:(UIViewVerticalAlignment) verticalAlignment;
+
+- (void) positionSubviewAtTop:(UIView *) view withHeight:(CGFloat) height;
+- (void) positionSubviewAtBottom:(UIView *) view withHeight:(CGFloat) height;
+- (void) positionSubviewAtLeft:(UIView *) view withWidth:(CGFloat) width;
+- (void) positionSubviewAtRight:(UIView *) view withWidth:(CGFloat) width;
+
+- (void) positionSubviewFull:(UIView *) view;
+
+- (void) addSubview:(UIView *) view horizontalAlignment:(UIViewHorizontalAlignment) horizontalAlignment verticalAlignment:(UIViewVerticalAlignment) verticalAlignment width:(CGFloat) width height:(CGFloat) height;
+- (void) addSubview:(UIView *) view horizontalAlignment:(UIViewHorizontalAlignment) horizontalAlignment verticalAlignment:(UIViewVerticalAlignment) verticalAlignment;
+
+- (void) addSubviewToTop:(UIView *) view withHeight:(CGFloat) height;
+- (void) addSubviewToBottom:(UIView *) view withHeight:(CGFloat) height;
+- (void) addSubviewToLeft:(UIView *) view withWidth:(CGFloat) width;
+- (void) addSubviewToRight:(UIView *) view withWidth:(CGFloat) width;
+
+- (void) addSubviewFull:(UIView *) view;
+
+- (NSString *) debugHierarchy:(CGPoint) pointToCheck;
+- (NSString *) debugString;
+
+- (UIImage *) captureContentAsImage;
 
 @end
--- LibClasses/UIView+IVGUtils.m
+++ LibClasses/UIView+IVGUtils.m
@@ -7,8 +7,12 @@
 //
 
 #import "UIView+IVGUtils.h"
+#import <QuartzCore/QuartzCore.h>
+#import <objc/runtime.h>
 
 @implementation UIView(IVGUtils)
+
+static NSString *VIEW_OWNER_ASSOCOBJ_KEY = @"com.ivygulch.VIEW_OWNER_KEY";
 
 - (NSString *) superviewDescription {
     NSString *result = @"";
@@ -24,8 +28,8 @@
     return result;
 }
 
-- (id) findSuperviewIsKindOfClass: (Class) clazz {
-    UIView *v = self.superview;
+- (id) findFirstViewInHierarchyWithKindOfClass: (Class) clazz {
+    UIView *v = self;
     while (v) {
         if ([v isKindOfClass:clazz]) {
             return v;
@@ -48,14 +52,6 @@
     NSArray *recognizers = [NSArray arrayWithArray:self.gestureRecognizers];
     for (UIGestureRecognizer *recognizer in recognizers) {
         [self removeGestureRecognizer:recognizer];
-    }
-}
-
-- (void) removeAllSubviews;
-{
-    NSArray *subviews = [NSArray arrayWithArray:self.subviews];
-    for (UIView *subview in subviews) {
-        [subview removeFromSuperview];
     }
 }
 
@@ -115,5 +111,339 @@
     return (top == CGFLOAT_MAX) ? 0 : top;
 }
 
+- (void) removeAllSubviews;
+{
+    NSArray *subviews = self.subviews;
+    for (UIView *subview in subviews) {
+        [subview removeFromSuperview];
+    }
+}
+
++ (id) findFirstViewInHiearchyWith:(id) item kindOfClass: (Class) clazz;
+{
+    if ([item isKindOfClass:[UIView class]]) {
+        return [item findFirstViewInHierarchyWithKindOfClass:clazz];
+    } else {
+        return nil;
+    }
+}
+
+- (CGRect) absoluteFrame;
+{
+    CGRect frame = self.frame;
+    frame.origin = [self absoluteOrigin];
+    return frame;
+}
+
+- (CGPoint) absoluteOrigin;
+{
+    CGPoint origin = self.frame.origin;
+    UIView *view = self.superview;
+    while (view != nil) {
+        origin.x += view.frame.origin.x;
+        origin.y += view.frame.origin.y;
+        view = view.superview;
+    }
+    return origin;
+}
+
+- (UIView *) findFirstResponder;
+{
+    if (self.isFirstResponder) {
+        return self;
+    }
+    for (UIView *subview in self.subviews) {
+        UIView *result = [subview findFirstResponder];
+        if (result) {
+            return result;
+        }
+    }
+    return nil;
+}
+
+- (id) viewOwnerFor:(UIView *) view;
+{
+    return objc_getAssociatedObject(view, (__bridge const void *) VIEW_OWNER_ASSOCOBJ_KEY);
+}
+
+- (id) viewOwner;
+{
+    UIView *view = self;
+    while (view != nil) {
+        id result = [self viewOwnerFor:view];
+        if (result) {
+            return result;
+        }
+        view = view.superview;
+    }
+    return nil;
+}
+
+- (void) setViewOwner:(id) viewOwner;
+{
+    // needs to be a weak reference so we avoid a retain cycle between the view and it's controller/owner
+    objc_setAssociatedObject(self, (__bridge const void *)  VIEW_OWNER_ASSOCOBJ_KEY, viewOwner, OBJC_ASSOCIATION_ASSIGN);
+}
+
+- (void) positionSubview:(UIView *) view horizontalAlignment:(UIViewHorizontalAlignment) horizontalAlignment verticalAlignment:(UIViewVerticalAlignment) verticalAlignment width:(CGFloat) width height:(CGFloat) height;
+{
+    CGSize svSize = self.bounds.size;
+    CGFloat x = view.frame.origin.x;
+    CGFloat y = view.frame.origin.y;
+    CGFloat w = width;
+    CGFloat h = height;
+    UIViewAutoresizing autoresizingMask = UIViewAutoresizingNone;
+
+    switch (verticalAlignment) {
+        case UIViewVerticalAlignmentNone: {
+            break;
+        }
+        case UIViewVerticalAlignmentTop: {
+            y = 0;
+            autoresizingMask |= UIViewAutoresizingFlexibleBottomMargin;
+            break;
+        }
+        case UIViewVerticalAlignmentCenter: {
+            y = (svSize.height - h) / 2;
+            break;
+        }
+        case UIViewVerticalAlignmentFull: {
+            y = 0;
+            h = svSize.height;
+            autoresizingMask |= UIViewAutoresizingFlexibleHeight;
+            break;
+        }
+        case UIViewVerticalAlignmentBottom: {
+            y = svSize.height - h;
+            autoresizingMask |= UIViewAutoresizingFlexibleTopMargin;
+            break;
+        }
+    }
+    
+    switch(horizontalAlignment) {
+        case UIViewHorizontalAlignmentNone: {
+            break;
+        }
+        case UIViewHorizontalAlignmentLeft: {
+            x = 0;
+            autoresizingMask |= UIViewAutoresizingFlexibleRightMargin;
+            break;
+        }
+        case UIViewHorizontalAlignmentCenter: {
+            x = (svSize.width - w) / 2;
+            break;
+        }
+        case UIViewHorizontalAlignmentFull: {
+            x = 0;
+            w = svSize.width;
+            autoresizingMask |= UIViewAutoresizingFlexibleWidth;
+            break;
+        }
+        case UIViewHorizontalAlignmentRight: {
+            x = svSize.width - w;
+            autoresizingMask |= UIViewAutoresizingFlexibleLeftMargin;
+            break;
+        }
+    }
+    
+    view.autoresizingMask = autoresizingMask;
+    view.frame = CGRectMake(x,y,w,h);
+}
+
+- (void) positionSubview:(UIView *) view horizontalAlignment:(UIViewHorizontalAlignment) horizontalAlignment verticalAlignment:(UIViewVerticalAlignment) verticalAlignment;
+{
+    [self positionSubview:view horizontalAlignment:horizontalAlignment verticalAlignment:verticalAlignment width:view.bounds.size.width height:view.bounds.size.height];
+}
+
+- (void) positionSubviewAtTop:(UIView *) view withHeight:(CGFloat) height;
+{
+    [self positionSubview:view horizontalAlignment:UIViewHorizontalAlignmentFull verticalAlignment:UIViewVerticalAlignmentTop width:0 height:height];
+}
+
+- (void) positionSubviewAtBottom:(UIView *) view withHeight:(CGFloat) height;
+{
+    [self positionSubview:view horizontalAlignment:UIViewHorizontalAlignmentFull verticalAlignment:UIViewVerticalAlignmentBottom width:0 height:height];
+}
+
+- (void) positionSubviewAtLeft:(UIView *) view withWidth:(CGFloat) width;
+{
+    [self positionSubview:view horizontalAlignment:UIViewHorizontalAlignmentLeft verticalAlignment:UIViewVerticalAlignmentFull width:width height:0];
+}
+
+- (void) positionSubviewAtRight:(UIView *) view withWidth:(CGFloat) width;
+{
+    [self positionSubview:view horizontalAlignment:UIViewHorizontalAlignmentRight verticalAlignment:UIViewVerticalAlignmentFull width:width height:0];
+}
+
+- (void) positionSubviewFull:(UIView *) view;
+{
+    [self positionSubview:view horizontalAlignment:UIViewHorizontalAlignmentFull verticalAlignment:UIViewVerticalAlignmentFull];
+}
+
+- (void) addSubview:(UIView *) view horizontalAlignment:(UIViewHorizontalAlignment) horizontalAlignment verticalAlignment:(UIViewVerticalAlignment) verticalAlignment width:(CGFloat) width height:(CGFloat) height;
+{
+    [self positionSubview:view horizontalAlignment:horizontalAlignment verticalAlignment:verticalAlignment width:width height:height];
+    [self addSubview:view];
+}
+
+- (void) addSubview:(UIView *) view horizontalAlignment:(UIViewHorizontalAlignment) horizontalAlignment verticalAlignment:(UIViewVerticalAlignment) verticalAlignment;
+{
+    [self positionSubview:view horizontalAlignment:horizontalAlignment verticalAlignment:verticalAlignment];
+    [self addSubview:view];
+}
+
+- (void) addSubviewToTop:(UIView *) view withHeight:(CGFloat) height;
+{
+    [self positionSubviewAtTop:view withHeight:height];
+    [self addSubview:view];
+}
+
+- (void) addSubviewToBottom:(UIView *) view withHeight:(CGFloat) height;
+{
+    [self positionSubviewAtBottom:view withHeight:height];
+    [self addSubview:view];
+}
+
+- (void) addSubviewToLeft:(UIView *) view withWidth:(CGFloat) width;
+{
+    [self positionSubviewAtLeft:view withWidth:width];
+    [self addSubview:view];
+}
+
+- (void) addSubviewToRight:(UIView *) view withWidth:(CGFloat) width;
+{
+    [self positionSubviewAtRight:view withWidth:width];
+    [self addSubview:view];
+}
+
+- (void) addSubviewFull:(UIView *) view;
+{
+    [self positionSubviewFull:view];
+    [self addSubview:view];
+}
+
+- (NSString *) debugString;
+{
+    return [NSString stringWithFormat:@"sz=%.0f,%.0f %.0f,%.0f",
+            self.frame.origin.x, self.frame.origin.y,
+            self.frame.size.width, self.frame.size.height];
+}
+
+- (void) appendDebugHierarchyTo:(NSMutableString *) ms withOffset:(CGPoint) offset pointToCheck:(CGPoint) pointToCheck margin:(NSString *) margin;
+{
+    CGRect f = self.frame;
+    CGRect fo = CGRectMake(f.origin.x+offset.x,f.origin.y+offset.y,f.size.width,f.size.height);
+
+    BOOL checkInside = !CGPointEqualToPoint(pointToCheck, CGPointZero);
+
+    BOOL inside = CGRectContainsPoint(fo,pointToCheck);
+    
+    if (!checkInside || inside) {
+        if ([ms length] > 0) {
+            [ms appendString:@"\n"];
+        }
+        
+        [ms appendString:margin];
+        [ms appendString:NSStringFromClass([self class])];
+        if (self.viewOwner) {
+            [ms appendString:@"/"];
+            [ms appendString:NSStringFromClass([self.viewOwner class])];
+        }
+        [ms appendFormat:@"<%p>  %.0f,%.0f,%.0f,%.0f (%.0f,%.0f,%.0f,%.0f)",
+         self,
+         f.origin.x, f.origin.y, f.size.width, f.size.height,
+         fo.origin.x, fo.origin.y, fo.size.width, fo.size.height
+         ];
+        if (checkInside) {
+            [ms appendFormat:@"  %.0f,%.0f", pointToCheck.x, pointToCheck.y];
+        }
+    }
+    NSString *nextMargin = [margin stringByAppendingString:@"  "];
+    CGPoint origin = self.frame.origin;
+    CGPoint nextOffset = CGPointMake(offset.x+origin.x,offset.y+origin.y);
+    for (UIView *sv in self.subviews) {
+        [sv appendDebugHierarchyTo:ms withOffset:nextOffset pointToCheck:pointToCheck margin:nextMargin];
+    }
+}
+
+- (NSString *) debugHierarchy:(CGPoint) pointToCheck;
+{
+    NSMutableString *ms = [NSMutableString string];
+    CGPoint origin = self.frame.origin;
+    [self appendDebugHierarchyTo:ms withOffset:CGPointMake(-origin.x,-origin.y) pointToCheck:pointToCheck margin:@""];
+    return ms;
+}
+
+- (UIImage *) captureContentAsImage;
+{
+    UIGraphicsBeginImageContext(self.frame.size);
+    [self.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *viewImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+
+    return viewImage;
+}
+
+#pragma mark - frame and bounds helper properties
+
+- (CGPoint) frameOrigin;
+{
+    return self.frame.origin;
+}
+
+- (void) setFrameOrigin:(CGPoint)frameOrigin;
+{
+    self.frame = (CGRect) {frameOrigin,self.frame.size};
+}
+
+- (CGFloat) frameX;
+{
+    return self.frameOrigin.x;
+}
+
+- (void) setFrameX:(CGFloat)frameX;
+{
+    self.frameOrigin = CGPointMake(frameX,self.frame.origin.y);
+}
+
+- (CGFloat) frameY;
+{
+    return self.frameOrigin.y;
+}
+
+- (void) setFrameY:(CGFloat)frameY;
+{
+    self.frameOrigin = CGPointMake(self.frame.origin.x,frameY);
+}
+
+- (CGSize) frameSize;
+{
+    return self.frame.size;
+}
+
+- (void) setFrameSize:(CGSize)frameSize;
+{
+    self.frame = (CGRect) {self.frame.origin,frameSize};
+}
+
+- (CGFloat) frameWidth;
+{
+    return self.frame.size.width;
+}
+
+- (void) setFrameWidth:(CGFloat)frameWidth;
+{
+    self.frameSize = CGSizeMake(frameWidth,self.frame.size.height);
+}
+
+- (CGFloat) frameHeight;
+{
+    return self.frame.size.height;
+}
+
+- (void) setFrameHeight:(CGFloat)frameHeight;
+{
+    self.frameSize = CGSizeMake(self.frame.size.width,frameHeight);
+}
 
 @end
